6a7ac2af3e4c266afa7185e24b5625fb3c5e23e2
Lucas Cassilha <lucaszawadneak@gmail.com>
2020-03-20 11:47:36 -0300

    Pet Health style

--- src/pages/Pet/Health/index.js
+++ src/pages/Pet/Health/index.js
@@ -1,8 +1,130 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+import {
+  Container,
+  Title,
+  TitleBox,
+  List,
+  Box,
+  LabelTitle,
+  TextBox,
+  ButtonBox,
+  IconHolder,
+  LabelSubtitle,
+  DateBox,
+  DateLabel,
+  ButtonBoxSmall,
+} from './styles';
 
 export default function Health() {
-  return <View />;
+  const [doctors, setDoctors] = useState([{ id: 1 }, { id: 2 }, { id: 3 }]);
+  const [appointments, setAppointments] = useState([
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+  ]);
+  const [surgeries, setSurgeries] = useState([{ id: 1 }, { id: 2 }, { id: 3 }]);
+
+  return (
+    <Container>
+      <TitleBox>
+        <Title>Doctors </Title>
+      </TitleBox>
+      <List
+        data={doctors}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <Box>
+            <TextBox>
+              <LabelTitle>Doutor Estranho</LabelTitle>
+              <LabelSubtitle>Animal Clinic</LabelSubtitle>
+            </TextBox>
+            <ButtonBox>
+              <IconHolder>
+                <Icon name="phone" color="#fff" size={20} />
+              </IconHolder>
+              <IconHolder>
+                <Icon name="trash-alt" color="#fff" size={20} />
+              </IconHolder>
+            </ButtonBox>
+          </Box>
+        )}
+      />
+      <TitleBox>
+        <Title>Appointments</Title>
+      </TitleBox>
+      <List
+        data={appointments}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <Box>
+            <TextBox>
+              <LabelTitle>Animal Clinic</LabelTitle>
+              <LabelSubtitle>Doutor Estranho</LabelSubtitle>
+              <DateBox>
+                <DateLabel>13/05/2000 - 14:00</DateLabel>
+              </DateBox>
+            </TextBox>
+            <ButtonBox>
+              <IconHolder>
+                <Icon name="phone" color="#fff" size={20} />
+              </IconHolder>
+              <IconHolder>
+                <Icon name="trash-alt" color="#fff" size={20} />
+              </IconHolder>
+            </ButtonBox>
+          </Box>
+        )}
+      />
+      <TitleBox>
+        <Title>Surgeries</Title>
+      </TitleBox>
+      <List
+        data={surgeries}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <Box>
+            <TextBox>
+              <LabelTitle>Castration</LabelTitle>
+              <LabelSubtitle>Animal Clinic</LabelSubtitle>
+              <DateBox>
+                <DateLabel>13/05/2000</DateLabel>
+              </DateBox>
+            </TextBox>
+            <ButtonBoxSmall>
+              <IconHolder>
+                <Icon name="trash-alt" color="#fff" size={20} />
+              </IconHolder>
+            </ButtonBoxSmall>
+          </Box>
+        )}
+      />
+      <TitleBox>
+        <Title>Problems</Title>
+      </TitleBox>
+      <List
+        data={surgeries}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <Box>
+            <TextBox>
+              <LabelTitle>Epilepsy</LabelTitle>
+              <LabelSubtitle>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                maximus lacus vitae viverra ultricies. Lorem ipsum dolor sit
+                amet, consectetur adipiscing elit.
+              </LabelSubtitle>
+            </TextBox>
+            <ButtonBoxSmall>
+              <IconHolder>
+                <Icon name="trash-alt" color="#fff" size={20} />
+              </IconHolder>
+            </ButtonBoxSmall>
+          </Box>
+        )}
+      />
+    </Container>
+  );
 }
--- src/pages/Pet/Health/styles.js
+++ src/pages/Pet/Health/styles.js
@@ -1,3 +1,77 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    alignItems: 'center',
+  },
+})``;
+
+export const TitleBox = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 10px;
+`;
+
+export const Title = styled.Text`
+  font-family: 'OpenSans-Regular';
+  font-size: 21px;
+  color: #eb3349;
+  margin-left: 10px;
+`;
+
+export const List = styled.FlatList.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+})`
+  margin: 10px 0px;
+`;
+
+export const Box = styled.View`
+  background-color: #eb3349;
+  align-self: stretch;
+  width: 300px;
+  border-radius: 4px;
+  margin: 0px 10px;
+  padding: 20px 15px;
+  flex-direction: row;
+`;
+
+export const TextBox = styled.View`
+  width: 80%;
+  justify-content: center;
+`;
+
+export const LabelTitle = styled.Text`
+  color: #fff;
+  font-family: 'OpenSans-Bold';
+  font-size: 16px;
+`;
+
+export const LabelSubtitle = styled.Text`
+  font-family: 'OpenSans-Regular';
+  color: #fff;
+`;
+
+export const ButtonBox = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 20%;
+`;
+
+export const ButtonBoxSmall = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 30%;
+`;
+
+export const IconHolder = styled.TouchableOpacity`
+  margin-right: 10px;
+`;
+
+export const DateBox = styled.View``;
+
+export const DateLabel = styled.Text`
+  color: #fff;
+`;
