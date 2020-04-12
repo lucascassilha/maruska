f07a1d2cea2d4d54bc863cd3c592f5f0f186d012
Lucas Cassilha <lucaszawadneak@gmail.com>
2020-04-12 16:30:33 -0300

    AdFix v2

--- src/App.js
+++ src/App.js
@@ -6,14 +6,9 @@ import './config/ReactotronConfig';
 import CodePush from 'react-native-code-push';
 import OneSignal from 'react-native-onesignal';
 
-import { AdMobInterstitial } from 'react-native-admob';
-
 import { store, persistor } from '~/store/index';
 
 import Routes from './routes';
-
-AdMobInterstitial.setAdUnitID('ca-app-pub-7615541994083029/3957454243');
-AdMobInterstitial.requestAd().then(() => AdMobInterstitial.showAd());
 
 function App() {
   OneSignal.init('3a5cc85d-1eb6-4169-bfee-920ecfd410d0', {
