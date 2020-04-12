df46f07876751cb745ebadc7d4297a60bc5ea130
Lucas Cassilha <lucaszawadneak@gmail.com>
2020-04-12 14:09:21 -0300

    GP ad fix

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import './config/ReactotronConfig';
 import CodePush from 'react-native-code-push';
 import OneSignal from 'react-native-onesignal';
 
+import { AdMobInterstitial } from 'react-native-admob';
+
 import { store, persistor } from '~/store/index';
 
 import Routes from './routes';
 
-console.disableYellowBox = true;
+AdMobInterstitial.setAdUnitID('ca-app-pub-7615541994083029/3957454243');
+AdMobInterstitial.requestAd().then(() => AdMobInterstitial.showAd());
 
 function App() {
   OneSignal.init('3a5cc85d-1eb6-4169-bfee-920ecfd410d0', {
     kOSSettingsKeyAutoPrompt: true,
   });
+
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
